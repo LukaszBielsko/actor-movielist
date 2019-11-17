82069bd52bd2af78871a1d172b44bd31c52f2376
Lukasz Walczak <lukeabove@gmail.com>
2019-11-17 18:46:51 +0100

    Jest and Enzyme added.
    Together with enzyme jest and enzyme adapter.

--- src/views/App.test.js
+++ src/views/App.test.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new EnzymeAdapter()})
+
+it('renders withour crashing', () => {
+  const wrapper = shallow(<App/>) 
+  console.log(wrapper.debug())
+  expect(wrapper).toBeTruthy()
+})
+
+
+/* Original test */
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
