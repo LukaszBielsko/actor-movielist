1d54601d43188009e20be92ec00fd8c4bf569989
Lukasz Walczak <lukeabove@gmail.com>
2019-11-21 09:00:38 +0100

    Test added for rendering AppHeader (static elements only)
    Movie list that require async data fetching was not tested.

--- src/views/components/AppHeader/AppHeader.js
+++ src/views/components/AppHeader/AppHeader.js
@@ -4,6 +4,11 @@ import * as actionCreators from '../../../store/actions';
 import PropTypes from 'prop-types';
 
 
+/* TODO using hooks create flag for actors dropdown list
+    onChange = true
+    onClick =false 
+*/
+
 const AppHeader = (props) => {
 
     let actorsList = null;
@@ -22,13 +27,15 @@ const AppHeader = (props) => {
     return (
         <>
             <div data-test="app-header" className="flex  w-2/3 m-auto h-12">
-                
+
                 <div
                     className="bg-gray-900 border-1 border-black border-2 rounded text-white py-2 px-4 mr-auto font-bold">
                     GITHUB
                 </div>
 
-                <div className="border-black border-2 rounded bg-gray-900 p-2 text-white font-bold flex" >
+                <div 
+                data-test="search-box"
+                className="border-black border-2 rounded bg-gray-900 p-2 text-white font-bold flex" >
 
                     <span>???</span>
                     <div className="flex flex-col">
@@ -38,9 +45,11 @@ const AppHeader = (props) => {
                             onChange={e => props.getSearchQuery(e.target.value)}
                             type="text"
                             className="bg-gray-900 ml-2 font-bold text-white "
-                            placeholder="search / dropdown list">
+                            placeholder="...search...">
                         </input>
-                        <div className="bg-gray-900 z-10 rounded-lg ">
+                        <div
+                            data-test="actors-list"
+                            className="bg-gray-900 z-10 rounded-lg">
                             {actorsList}
                         </div>
                     </div>
--- src/views/components/AppHeader/AppHeader.test.js
+++ src/views/components/AppHeader/AppHeader.test.js
@@ -1,30 +1,59 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 
 import AppHeader from './AppHeader';
+import { findByTestAttr, storeFactory } from '../../../testUtils';
 
-import  {shallow} from 'enzyme';
+const setup = (props = {}, state = null) => {
+    const store = storeFactory()
+    return shallow(<AppHeader store={store} {...props} />).dive().dive()
+}
 
+describe('render', () => {
+    describe('app header', () => {
+        test('renders app header div', () => {
+            const wrapper = setup()
+            const header = findByTestAttr(wrapper, 'app-header')
+            expect(header.length).toBe(1)
+        });
 
-const setup = (props = {}, state = null) => wrapper = shallow(<AppHeader {...props} />)
+        test('renders search box', () => {
+            const wrapper = setup()
+            const searchBox = findByTestAttr(wrapper, 'search-box')
+            expect(searchBox.length).toBe(1)
+        });
 
-const findByTestAttr = (wrapper, selector) => wrapper.find(`[data-test='${selector}']`)  
+        test('renders input field', () => {
+            const wrapper = setup()
+            const input = findByTestAttr(wrapper, 'input-field')
+            expect(input.length).toBe(1)
+        });
+    })
+})
 
-describe('renders elements properly', () => {
-    test('renders app header div', () => {
-//         const wrapper = setup()
-//         const header = findByTestAttr(wrapper, 'app-header')
-//         expect(header.length).toBe(1)
-    });
+describe('update state', () => {
+
 })
 
 /*
    Invariant Violation: Could not find "store" in the context of "Connect(AppHeader)". Either wrap the root component in a <Provider>, or pass a custom React context provider to <Provider> and the corresponding React context consumer to Connect(AppHeader) in connect options.
-
+w
        8 | Enzyme.configure({ adapter: new EnzymeAdapter() })
        9 |
     > 10 | const setup = (props = {}, state = null) => wrapper = shallow(<AppHeader {...props} />)
          |                                                       ^
       11 |
       12 | const findByTestAttr = (wrapper, selector) => wrapper.find(`[data-test='${selector}']`)
-      13 | 
+      13 |
+
+solution => create storeFactory function in testUtils.js that creates store and pass it as a prop => but then we have dive two level as we get
+
+    const setup = (initialState = {}) => {
+        const store = storeFactory(initialState)
+        const deepDiveWrapper = shallow(<Input store={store} />).dive().dive();
+        // console.log(shallow(<Input store={store} />).debug())
+        // console.log(deepDiveWrapper)
+        return deepDiveWrapper
+    }
+
 */
